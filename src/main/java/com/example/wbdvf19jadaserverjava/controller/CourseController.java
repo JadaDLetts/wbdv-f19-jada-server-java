33c1ebd87bb298af9a9f5e90598779a913126a47
Jada Letts <45326648+JadaDLetts@users.noreply.github.com>
2019-11-12 23:19:19 -0500

    updated Course Controller

--- src/main/java/com/example/wbdvf19jadaserverjava/controller/CourseController.java
+++ src/main/java/com/example/wbdvf19jadaserverjava/controller/CourseController.java
@@ -3,6 +3,6 @@ package com.example.wbdvf19jadaserverjava.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("https://tranquil-refuge-56291.herokuapp.com/")
 public class CourseController {
 }
