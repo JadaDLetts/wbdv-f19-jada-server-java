3a139404f80f1b3698ac12f7ea1e6b956bfb82d2
Jada Letts <45326648+JadaDLetts@users.noreply.github.com>
2019-11-15 14:15:05 -0500

    updated Course Model

--- src/main/java/com/example/wbdvf19jadaserverjava/controller/WidgetController.java
+++ src/main/java/com/example/wbdvf19jadaserverjava/controller/WidgetController.java
@@ -1,8 +1,54 @@
 package com.example.wbdvf19jadaserverjava.controller;
 
+import com.example.wbdvf19jadaserverjava.model.Widget;
+import com.example.wbdvf19jadaserverjava.services.WidgetService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@CrossOrigin(origins="http://localhost:3000",allowCredentials="true",allowedHeaders="*")
+@CrossOrigin(origins="*",allowCredentials="true",allowedHeaders="*")
 public class WidgetController {
+
+    WidgetService widgetService = new WidgetService();
+
+//    List<Widget> widgets = new ArrayList<Widget>();
+//    {
+//        widgets.add(new Widget(123L, "Widget 1", "HEADING"));
+//        widgets.add(new Widget(234L, "Widget 2", "PARAGRAPH"));
+//        widgets.add(new Widget(345L, "Widget 3", "LINK"));
+//        widgets.add(new Widget(456L, "Widget 4", "IMAGE"));
+//        widgets.add(new Widget(567L, "Widget 5", "LIST"));
+//    }
+
+    @PostMapping("/api/courses/{courseId}/widgets")
+    public List<Widget> createWidget(
+            @PathVariable("courseId") long courseId) {
+        widgetService.createWidget(courseId);
+        return widgetService.findAllWidgets(courseId);
+    }
+
+    @GetMapping("/api/courses/{courseId}/widgets")
+    public List<Widget> findAllWidgets(
+            @PathVariable("courseId") long courseId){
+               return widgetService.findAllWidgets(courseId);
+    }
+
+    @GetMapping("/api/courses/{courseId}/widgets/{widgetId}")
+    public Widget findWidgetById(
+            @PathVariable("widgetId") long widgetId,
+            @PathVariable("courseId") long courseId) {
+        return widgetService.findWidgetById(courseId, widgetId);
+    }
+
+    @DeleteMapping("/api/courses/{courseId}/widgets/{widgetId}")
+    public List<Widget> deleteWidget(
+            @PathVariable("widgetId") long widgetId,
+            @PathVariable("courseId") long courseId,
+            @RequestBody String type) {
+        widgetService.setWidgetType(courseId, widgetId, type);
+        return widgetService.findAllWidgets(courseId);
+    }
 }
--- src/main/java/com/example/wbdvf19jadaserverjava/model/Course.java
+++ src/main/java/com/example/wbdvf19jadaserverjava/model/Course.java
@@ -1,9 +1,21 @@
 package com.example.wbdvf19jadaserverjava.model;
 
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Course {
 	private String title;
     private long id;
+    private List<Widget> widgets;
+
+    public Course(long id, String title) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.widgets = new ArrayList<Widget>();
+    }
 
     public long getId() {
         return this.id;
@@ -21,10 +33,35 @@ public class Course {
         this.title = title;
     }
 
-    public Course() {}
-    public Course(long id, String title) {
-        super();
-        this.id = id;
-        this.title = title;
+    public List<Widget> getWidgets() {
+        return widgets;
     }
+
+
+    public void setWidgetType(long widgetId, Type type) {
+        for (Widget widget : this.widgets) {
+            if (widget.getId() == widgetId) {
+                widget.setType(type);
+            }
+        }
+    }
+
+    public void addWidget(Widget widget) {
+        this.widgets.add(widget);
+    }
+    public void deleteWidget(Widget widget) {
+        List<Widget> filtered = new ArrayList<>();
+        for (Widget wid : this.widgets) {
+            if (wid.getId() != widget.getId()) {
+                filtered.add(wid);
+            }
+        }
+        this.widgets = filtered;
+    }
+
+    public void createWidget() {
+        this.widgets.add(new Widget((int)new Date().getTime(), "New Widget", Type.HEADING));
+    }
+
+
 }
