a1702e4f3127d0d924eaed436966b6688084fcc6
Jada Letts <45326648+JadaDLetts@users.noreply.github.com>
2019-11-15 14:16:20 -0500

    updated WIdget Service

--- src/main/java/com/example/wbdvf19jadaserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvf19jadaserverjava/services/WidgetService.java
@@ -1,64 +1,60 @@
 package com.example.wbdvf19jadaserverjava.services;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.example.wbdvf19jadaserverjava.model.Widget;
-import org.springframework.web.bind.annotation.*;
+import com.example.wbdvf19jadaserverjava.model.Type;
 
-@RestController
-@CrossOrigin(origins="http://localhost:3000",allowCredentials="true",allowedHeaders="*")
 public class WidgetService {
-    List<Widget> widgets = new ArrayList<Widget>();
-    {
-        widgets.add(new Widget(123L, "Widget 1", "HEADING"));
-        widgets.add(new Widget(234L, "Widget 2", "PARAGRAPH"));
-        widgets.add(new Widget(345L, "Widget 3", "LINK"));
-        widgets.add(new Widget(456L, "Widget 4", "IMAGE"));
-        widgets.add(new Widget(567L, "Widget 5", "LIST"));
+    private CourseService courseService;
+
+    public WidgetService() {
+        this.courseService = new CourseService();
     }
 
-    @PutMapping("/api/widgets/{widgetId}")
-    public List<Widget> updateWidget(
-            @PathVariable("widgetId") long id,
-            @RequestBody Widget newWidget) {
-        for (Widget widget: widgets) {
-            if(widget.getId() == id) {
-                widget.setTitle(newWidget.getTitle());
-            }
+    public void setWidgetType(long courseId, long widgetId, String s) {
+        Type t = null;
+        switch(s) {
+            case "HEADING":
+                t = Type.HEADING;
+                break;
+            case "LINK":
+                t = Type.LINK;
+                break;
+            case "IMAGE":
+                t = Type.IMAGE;
+                break;
+            case "LIST":
+                t = Type.LIST;
+                break;
+            case "PARAGRAPH":
+                t = Type.PARAGRAPH;
+                break;
+            default:
+                break;
         }
-        return widgets;
+        this.courseService.setWidgetType(courseId, widgetId, t);
     }
 
-    @PostMapping("/api/widgets")
-    public List<Widget> createWidget(
-            @RequestBody Widget widget) {
-        widgets.add(widget);
-        return widgets;
+    public List<Widget> findAllWidgets(long courseId){
+        return this.courseService.findAllWidgets(courseId);
     }
 
-    @GetMapping("/api/widgets")
-    public List<Widget> findAllWidgets() {
-        return widgets;
+    public void createWidget(long courseId) {
+        this.courseService.createWidget(courseId);
     }
 
-    @GetMapping("/api/widgets/{widgetId}")
-    public Widget findWidgetById(
-            @PathVariable("widgetId") long id) {
-        for (Widget widget : widgets) {
-            if (widget.getId() == id) {
-                return widget;
+    public void deleteWidget(long courseId, long widgetId) {
+        this.courseService.deleteWidget(courseId, widgetId);
+    }
+
+    public Widget findWidgetById(long courseId, long widgetId) {
+        List<Widget> widgets = this.courseService.findAllWidgets(courseId);
+        for(Widget wid : widgets) {
+            if( wid.getId() == widgetId) {
+                return wid;
             }
         }
         return null;
     }
 
-    @DeleteMapping("/api/widgets/{widgetId}")
-    public List<Widget> deleteWidget(@PathVariable("widgetId") long wid) {
-        widgets = widgets
-                .stream()
-                .filter(widget -> !(widget.getId() == wid))
-                .collect(Collectors.toList());
-        return widgets;
-    }
 }
